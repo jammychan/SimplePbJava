993ae7d6b068b09d96ea996bdfa5a39fccd7dbed
jammychan <chenzemin23@gmail.com>
2022-07-07 15:56:12 +0800

    refactor code

--- src/CodeGenerator.ts
+++ src/CodeGenerator.ts
@@ -1,8 +1,54 @@
-import { ProtoFile } from "./ProtoFile"
+import * as protobuf from 'protobufjs'
+import { IO } from "./IO"
+import { ParseHelper } from './ParseHelper'
+
 
 export class CodeGenerator {
     
-    public generate(protoFile: ProtoFile, javaOutDir: string) {
+    public generate(protoPath: string, protosDir: string, javaOutDir: string) {
+        IO.mkdir(javaOutDir)
+
+        // 把protobufjs解析的json,转换成ProtoFile
+        let pathToProtoResult: any = {}
+        this.parseCurProto(protoPath, protosDir, pathToProtoResult)
+        // console.log(JSON.stringify(pathToProtoResult))
+        let protoFile = new ParseHelper().toProtoFile(pathToProtoResult[protoPath], pathToProtoResult)
+        
+        // generate java source codee
         protoFile.generateCode(javaOutDir)
+    }
+
+    protected parseCurProto(protoPath: string, protosDir: string, pathToProtoResult: any, depth: number = 0) {
+        if (depth > 1) {
+            // A import B, B import C, C import D, just parse A/B, abandon C/D
+            return
+        }
+        if (pathToProtoResult[protoPath] != null) {
+            return
+        }
+        const root = new protobuf.Root()
+        let importedPaths: string[] = []
+        root.resolvePath = function(origin, target) {
+            // origin is the path of the importing file
+            // target is the path of the imported file in origin
+            // ... determine absolute path and return it ...
+            let ret: string | null = ''
+            if (origin == null || origin.length == 0 || protosDir == null) {
+                ret = protoPath == target ? target : null
+            } else {
+                // 引用的proto不解析, 只记录好路劲
+                let importedPath = `${protosDir}/${target}`
+                importedPaths.push(importedPath)
+                
+                ret = null
+            }
+            // console.log(`origin=${origin}, target=${target}, ret=${ret}`)
+            return ret
+        }
+        let parseResult = root.loadSync(protoPath)
+        pathToProtoResult[protoPath] = parseResult
+        for (var path of importedPaths) {
+            this.parseCurProto(path, protosDir, pathToProtoResult, depth+1)
+        }
     }
 }
--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,6 @@
-import * as protobuf from 'protobufjs'
 import { CodeGenerator } from './CodeGenerator'
-import { ParseHelper } from './ParseHelper'
-import { IO } from './IO'
 
 (async function main() {
-
     // protoc --proto_path=./protos --java_out=./proto_java_old ./protos/namecard.proto
     // 解析参数、创建文件夹
     var argv = require('process-argv')({single_hyphen_multioption: false})
@@ -13,55 +9,14 @@ import { IO } from './IO'
         printHelp()
         process.exit(1)
     }
-    let javaOutDir = argv.options && argv.options.java_out || '.'
-    IO.mkdir(javaOutDir)
+    let protoPath  = argv.params[0]  // only one proto file one time
     let protosDir  = argv.options && argv.options.proto_path || null
+    let javaOutDir = argv.options && argv.options.java_out || '.'
 
-    // 把protobufjs解析的json,转换成ProtoFile
-    let pathToProtoResult: any = {}
-    parseCurProto(argv.params, protosDir, pathToProtoResult)
-    // console.log(JSON.stringify(pathToProtoResult))
-    let protoFile = new ParseHelper().toProtoFile(pathToProtoResult[argv.params], pathToProtoResult)
-
-    // 传入code generator生成java
-    new CodeGenerator().generate(protoFile, javaOutDir)
+    new CodeGenerator().generate(protoPath, protosDir, javaOutDir)
 })().catch((err: any) => {
     console.error(err)
 })
-
-function parseCurProto(protoPath: string, protosDir: string, pathToProtoResult: any, depth: number = 0) {
-    if (depth > 1) {
-        // A import B, B import C, C import D, just parse A/B, abandon C/D
-        return
-    }
-    if (pathToProtoResult[protoPath] != null) {
-        return
-    }
-    const root = new protobuf.Root()
-    let importedPaths: string[] = []
-    root.resolvePath = function(origin, target) {
-        // origin is the path of the importing file
-        // target is the path of the imported file in origin
-        // ... determine absolute path and return it ...
-        let ret: string | null = ''
-        if (origin == null || origin.length == 0 || protosDir == null) {
-            ret = protoPath == target ? target : null
-        } else {
-            // 引用的proto不解析, 只记录好路劲
-            let importedPath = `${protosDir}/${target}`
-            importedPaths.push(importedPath)
-            
-            ret = null
-        }
-        // console.log(`origin=${origin}, target=${target}, ret=${ret}`)
-        return ret
-    }
-    let parseResult = root.loadSync(protoPath)
-    pathToProtoResult[protoPath] = parseResult
-    for (var path of importedPaths) {
-        parseCurProto(path, protosDir, pathToProtoResult, depth+1)
-    }
-}
 
 function printHelp() {
     console.log(`Please input proto path.`)
